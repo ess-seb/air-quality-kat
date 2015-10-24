85c43cc5015fb62b05d4f65f05e76b7eaf0f6543
Sebastian <ess.seb@gmail.com>
2015-10-24 16:31:03 +0200

    v1

--- scraper.py
+++ scraper.py
@@ -1,52 +1,45 @@
 from datetime import date, timedelta
 from time import strftime, sleep
 from random import randint
-from urllib import urlencode
 from sys import argv
 import json
-import urllib2
-# Local settings
+import urllib
+import urllib.request
+
 import settings
 
-# A station name must be given
-if len(argv) < 2 or not settings.STATIONS.get(argv[1], False):
-	# Human readable stations list
-	stations = ', '.join( settings.STATIONS.keys() )
-	# Print out the available stations
-	print 'You must specify the station: %s' % stations
-	# End now
+if len(argv) < 2 or not settings.STATIONS.get(argv[1], False):	
+	stations = ', '.join( settings.STATIONS.keys() )	
+	print('You must specify the station: %s' % stations)	
 	exit()
 
-# Get the station name from command arguments
+
 station_name = argv[1]
-# Date we need in format DD.MM.YYYY, defaults is yesterday
 for i in range(1,357):
-	yesterday = date.today() - timedelta(i)
-	# The page we need to scrape
+	yesterday = date.today() - timedelta(i)	
 	url = "http://powietrze.katowice.wios.gov.pl/dane-pomiarowe/pobierz"
-	# Query parameters
+	
 	params = {
 		"measType":"Auto",
 		"viewType":"Station",
 		"dateRange":"Day",
 		"date": yesterday.strftime("%d.%m.%Y")
 	}
-	# Get the station we're looking for
-	station = settings.STATIONS[station_name]
-	# Update the query parameters with the station
-	params.update( station['query'] )
-	# The POST data we'll send
-	body = urlencode({ 'query': json.dumps(params) })
-	# Pause
+	
+	station = settings.STATIONS[station_name]	
+	params.update( station['query'] )	
+	data = urllib.parse.urlencode({ 'query': json.dumps(params) })
+	data = data.encode('utf-8')
+
 	sleep(randint(1,3))
-	# Sends the request 
-	req = urllib2.Request(url, body)
-	response = urllib2.urlopen(req)
-	# Converts the response to JSON
-	response_json = json.loads(response.read())
-	# Parse the JSON to a CSV file
+	req = urllib.request.Request(url, data)
+	resp = urllib.request.urlopen(req)
+	respData = resp.read()
+	
+	response_json = json.loads(respData.decode("utf-8"))
+	
 	for series in response_json["data"]["series"]:
 		series_id = series["paramId"]
 		for data in series["data"]:
-			# Bear in mind that data[0] is a UNIX epoch timestamp
-			print ','.join([ str(station_name), str(series_id), str(data[0]), str(data[1]) ])
+			
+			print(','.join([ str(station_name), str(series_id), str(data[0]), str(data[1]) ]))
